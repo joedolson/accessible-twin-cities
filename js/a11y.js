e5bb09564840463a6c8b845833a1f8bb9bdc3e82
joedolson <design@joedolson.com>
2014-06-10 15:41:50 -0500

    Just a note.
    a11y.js

--- js/a11y.js
+++ js/a11y.js
@@ -4,6 +4,7 @@
 	$('a').removeAttr('target');
 	
 	// add target attribute when rel=external
+	// @why There are good reasons for having links open in another window; this requires a higher level of knowledge to do it.
 	$('a[rel=external]').attr( 'target', '_blank' );
 	
 	// make dropdowns functional on focus
