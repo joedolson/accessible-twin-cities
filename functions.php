52febda4cc33f4b91d0716cb9a3037d72df23f4f
joedolson <design@joedolson.com>
2014-06-12 16:08:47 -0500

    Reorganization of functions.php
    Allow some functions to be overridden by child themes.

--- functions.php
+++ functions.php
@@ -1,62 +1,118 @@
 <?php
-register_sidebar( array(
-	'name'=>'Post Sidebar',
-	'description' => __( 'Widgets in this region will appear on all posts and post archives', 'accessible-twin-cities' ),
-	'id' => 'ps1',
-	'before_widget' => '<div id="%1$s" class="widget %2$s">',
-	'after_widget' => '</div>',
-	'before_title' => '<h2>',
-	'after_title' => '</h2>',
-));
+/**
+ * Capitol functions and definitions
+ * @package capitol
+ */
+add_action( 'after_setup_theme', 'capitol_setup' );
+if ( ! function_exists( 'capitol_setup' ) ) {
+	/**
+	 * Sets up theme defaults and registers support for various WordPress features.
+	 *
+	 * Note that this function is hooked into the after_setup_theme hook, which runs
+	 * before the init hook. The init hook is too late for some features, such as indicating
+	 * support post thumbnails.
+	 *
+	 * To override capitol_setup() in a child theme, add your own capitol_setup to your child theme's
+	 * functions.php file.
+	 *
+	 * @uses add_theme_support() To add support for post thumbnails and automatic feed links.
+	 * @uses register_nav_menus() To add support for navigation menus.
+	 * @uses add_custom_background() To add support for a custom background.
+	 * @uses add_editor_style() To style the visual editor.
+	 * @uses load_theme_textdomain() For translation/localization support.
+	 * @uses add_custom_image_header() To add support for a custom header.
+	 * @uses register_default_headers() To register the default custom header images provided with the theme.
+	 * @uses set_post_thumbnail_size() To set a custom post thumbnail size.
+	 *
+	 * @since Capitol 1.0
+	 */
+	function capitol_setup() {
+	
+		global $content_width;
+		if ( ! isset( $content_width ) ) {
+			$content_width = 600; 
+		}
 
-register_sidebar( array(
-	'name'=>'Home Sidebar',
-	'description' => __( 'Add up to 5 widgets to show on the bottom of your front page.', 'accessible-twin-cities' ),
-	'id' => 'ps2',
-	'before_widget' => '<div id="%1$s" class="widget %2$s">',
-	'after_widget' => '</div>',
-	'before_title' => '<h2>',
-	'after_title' => '</h2>',
-));
+		load_theme_textdomain( 'accessible-twin-cities', get_template_directory() . '/lang' );
 
-register_sidebar( array(
-	'name'=>'Page Sidebar',
-	'description' => __( 'Widgets in this region will appear on WordPress Pages.', 'accessible-twin-cities' ),
-	'id' => 'ps3',
-	'before_widget' => '<div id="%1$s" class="widget %2$s">',
-	'after_widget' => '</div>',
-	'before_title' => '<h2>',
-	'after_title' => '</h2>',
-));
+		add_theme_support( 'html5', array( 'comment-list', 'search-form', 'comment-form' ) );
+		add_theme_support( 'post-thumbnails' );
+		add_theme_support( 'post-formats', array( 'audio', 'gallery', 'image', 'video' ) );
+		add_theme_support( 'automatic-feed-links' ); 
+		add_theme_support( 'custom-header', apply_filters( 'atc_custom_header_args', array(
+				'default-color' => 'fff',
+				'width' => 960, 
+				'flex-width' => true,
+				'height' => 180,
+				'flex-height' => true,
+				'default-image' => get_template_directory_uri() . '/images/header.jpg',
+				'uploads' => true
+			) ) );
+		add_theme_support( 'custom-background', apply_filters( 'atc_custom_background_args', array(
+				'default-color' => 'f5f5f5',
+				'default-image' => '',
+			) ) );
 
-register_sidebar( array(
-	'name'=>'Global Sidebar - Top',
-	'description' => __( 'These widgets appear globally on posts and pages, excluding the front page.', 'accessible-twin-cities' ),
-	'id' => 'ps4',
-	'before_widget' => '<div id="%1$s" class="widget %2$s">',
-	'after_widget' => '</div>',
-	'before_title' => '<h2>',
-	'after_title' => '</h2>',
-));
+	}
+}
 
-register_sidebar( array(
-	'name'=>'Global Sidebar - Bottom',
-	'description' => __( 'These widgets appear globally on posts and pages, excluding the front page.', 'accessible-twin-cities' ),
-	'id' => 'ps5',
-	'before_widget' => '<div id="%1$s" class="widget %2$s">',
-	'after_widget' => '</div>',
-	'before_title' => '<h2>',
-	'after_title' => '</h2>',
-));
+add_action( 'widgets_init', 'capitol_widgets_init' );
+if ( ! function_exists( 'capitol_widgets_init' ) ) {
+	function capitol_widgets_init() {
+		register_sidebar( array(
+			'name'=>'Post Sidebar',
+			'description' => __( 'Widgets in this region will appear on all posts and post archives', 'accessible-twin-cities' ),
+			'id' => 'ps1',
+			'before_widget' => '<div id="%1$s" class="widget %2$s">',
+			'after_widget' => '</div>',
+			'before_title' => '<h2>',
+			'after_title' => '</h2>',
+		));
 
-load_theme_textdomain( 'accessible-twin-cities', get_template_directory() . '/lang' );
+		register_sidebar( array(
+			'name'=>'Home Sidebar',
+			'description' => __( 'Add up to 5 widgets to show on the bottom of your front page.', 'accessible-twin-cities' ),
+			'id' => 'ps2',
+			'before_widget' => '<div id="%1$s" class="widget %2$s">',
+			'after_widget' => '</div>',
+			'before_title' => '<h2>',
+			'after_title' => '</h2>',
+		));
 
+		register_sidebar( array(
+			'name'=>'Page Sidebar',
+			'description' => __( 'Widgets in this region will appear on WordPress Pages.', 'accessible-twin-cities' ),
+			'id' => 'ps3',
+			'before_widget' => '<div id="%1$s" class="widget %2$s">',
+			'after_widget' => '</div>',
+			'before_title' => '<h2>',
+			'after_title' => '</h2>',
+		));
+
+		register_sidebar( array(
+			'name'=>'Global Sidebar - Top',
+			'description' => __( 'These widgets appear globally on posts and pages, excluding the front page.', 'accessible-twin-cities' ),
+			'id' => 'ps4',
+			'before_widget' => '<div id="%1$s" class="widget %2$s">',
+			'after_widget' => '</div>',
+			'before_title' => '<h2>',
+			'after_title' => '</h2>',
+		));
+
+		register_sidebar( array(
+			'name'=>'Global Sidebar - Bottom',
+			'description' => __( 'These widgets appear globally on posts and pages, excluding the front page.', 'accessible-twin-cities' ),
+			'id' => 'ps5',
+			'before_widget' => '<div id="%1$s" class="widget %2$s">',
+			'after_widget' => '</div>',
+			'before_title' => '<h2>',
+			'after_title' => '</h2>',
+		));
+	}
+}
 // for demo purposes, I want this overridden by the inaccessible child; but will change after.
 require_once( get_stylesheet_directory() . '/inc/a11y.php' );
 //require_once( get_template_directory() . '/inc/a11y.php' );
-
-if ( ! isset( $content_width ) ) $content_width = 600; 
-if ( is_singular() ) wp_enqueue_script( "comment-reply" );
 
 add_filter( 'wp_title', 'atc_home_title' );
 function atc_home_title( $title ) {
@@ -67,24 +123,6 @@ function atc_home_title( $title ) {
 	}
 	return $title;
 }
-
-add_theme_support( 'html5', array( 'comment-list', 'search-form', 'comment-form' ) );
-add_theme_support( 'post-thumbnails' );
-add_theme_support( 'post-formats', array( 'audio', 'gallery', 'image', 'video' ) );
-add_theme_support( 'automatic-feed-links' ); 
-add_theme_support( 'custom-header', apply_filters( 'atc_custom_header_args', array(
-		'default-color' => 'fff',
-		'width' => 960, 
-		'flex-width' => true,
-		'height' => 180,
-		'flex-height' => true,
-		'default-image' => get_template_directory_uri() . '/images/header.jpg',
-		'uploads' => true
-	) ) );
-add_theme_support( 'custom-background', apply_filters( 'atc_custom_background_args', array(
-		'default-color' => 'f5f5f5',
-		'default-image' => '',
-	) ) );
 
 add_action( 'init', 'atc_add_editor_styles' );
 function atc_add_editor_styles() {
@@ -112,15 +150,6 @@ function atc_custom_header_image( $value ) {
 		// until header image customizer supports alt attributes, leave alt attribute blank.
 		echo "<img class='header-image' src='".get_header_image()."' width='".get_custom_header()->width."' height='".get_custom_header()->height."' alt='' />";
 	}
-}
-
-/* Probably can't do this */
-add_filter( 'the_title', 'atc_heading_title', 10, 2 );
-function atc_heading_title( $title, $id ) {
-	if ( get_post_meta( $id, 'h_title', true ) !== '' ) {
-		return get_post_meta( $id, 'h_title', true );
-	}
-	return $title;
 }
 
 add_action( 'wp_enqueue_scripts','atc_enqueue_scripts' );
