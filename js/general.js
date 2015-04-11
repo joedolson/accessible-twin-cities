0e1b4c69c7845a3c60536cf9ddc0cddf0dcfde82
joedolson <design@joedolson.com>
2015-04-11 16:00:51 -0500

    Fix bug with disappearing menu on mobile
    Mobile devices changed width whenever browser bars appeared, causing the
    menu to disappear. Also add support for aria-expanded toggling.

--- js/general.js
+++ js/general.js
@@ -1,16 +1,31 @@
 (function( $ ) { 'use strict';
 	// menu toggle
 	var width = $( window ).width();
-	if ( width <= 800 ) {
-		$( '.primary-menu nav ul').hide();
+	
+	if ( width < 800 ) {
+		$( '.primary-menu nav ul' ).hide();
 	}
-	$( window ).resize(function() {
-		var width = $( window ).width();
-		if ( width <= 800 ) {
-			$( '.primary-menu nav ul').hide();
+	
+	if ( window.innerWidth > 800 ) {
+		$( window ).resize(function() {
+			if ( $(this).width() != width ) {
+				var width = $( this ).width();
+				if ( width <= 800 ) {
+					$( '.primary-menu nav ul').hide();
+				} else {
+					$( '.primary-menu nav ul').show();
+				}
+			}
+		});
+	}
+	
+	$( '.menu-toggle' ).on( 'click', function() { 
+		$( '.primary-menu nav ul' ).toggle();
+		var visible = $( '.primary-menu nav ul' ).is( ':visible' );
+		if ( visible ) {
+			$(this).addClass( 'open' ).attr( 'aria-expanded', 'true' );
 		} else {
-			$( '.primary-menu nav ul').show();
+			$(this).removeClass( 'open' ).attr( 'aria-expanded', 'false' );
 		}
-	});
-	$( '.menu-toggle' ).on( 'click', function() { $( '.primary-menu nav ul' ).toggle() } );
+	} );
 }(jQuery));
