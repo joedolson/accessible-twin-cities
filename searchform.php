607d168f04c030414e7c47fc2811c772ae63dad0
joedolson <design@joedolson.com>
2015-04-11 16:01:00 -0500

    Prevent duplicate IDs

--- searchform.php
+++ searchform.php
@@ -1,8 +1,9 @@
 <div class="searchform">
 <form method="get" action="<?php echo esc_url( home_url( '/' ) ); ?>/" role="search">
+	<?php $form_id = rand( 100, 999 ); ?>
 	<p>
-	<label for="s" class='screen-reader-text'><?php _e( 'Search', 'universal' ); ?></label> 
-	<input type="text" name="s" id="s" placeholder="<?php _e( 'Search', 'universal' ); ?>" value="<?php echo trim( get_search_query() ); ?>" /> 
+	<label for="s<?php echo $form_id; ?>" class='screen-reader-text'><?php _e( 'Search', 'universal' ); ?></label> 
+	<input type="text" name="s" id="s<?php echo $form_id; ?>" placeholder="<?php _e( 'Search', 'universal' ); ?>" value="<?php echo trim( get_search_query() ); ?>" /> 
 	<input type="submit" name="submit" value="<?php _e( 'Search', 'universal' ); ?>" class="button" />
 	</p>
 </form>
